ac95b943942aff7273219cf0e6762ad06b5b3fb5
jihyunhillpark <girl9505@naver.com>
2021-02-14 00:32:00 +0900

    [#1463] 1로 만들기 - DP(Silver3 - 자잘한 오류가 있어 다시 upload)

--- Java/B1463.java
+++ Java/B1463.java
@@ -1,46 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class MakeOne {
+public class MakeOne2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// input
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int ret[][] = new int[n][3];
-
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		int ret[] = new int[n+1];
+		int min;
 		// DP - bottom up
-		// step1
-		int cnt = 0;
-		ret[0][0] = n;
-
 		// step2 and over
-		while(cnt < n && ret[cnt][0] !=1 && ret[cnt][1] !=1 && ret[cnt][2] !=1) {
-			int min = Integer.MAX_VALUE;
-			cnt++;
-			for(int i = 0 ; i < 3 ; i++) {
-				int pre = ret[cnt-1][i];
-				if( pre != 0 ) {
-					int curr = 0;
-					// 1 - 3으로 나눠지는 경우 check
-					if( pre%3 == 0) {
-						curr = ret[cnt][0];
-						if(curr == 0) ret[cnt][0] = pre/3;
-						else ret[cnt][0] = (curr < pre/3)? curr : pre/3;
-					}
-					// 2 - 2로 나눠지는 경우 check
-					if( pre%2 == 0) {
-						curr = ret[cnt][1];
-						if(curr == 0) ret[cnt][1] = pre/2;
-						else ret[cnt][1] = (curr < pre/2)? curr : pre/2;
-					}
-					// 3 - -1하는 경우
-					if(pre-1 < min ) min= pre-1;
+		for(int i = 1; i <= n ; i++) {
+			// step1 - base case
+			if(i == 1) ret[i] = 0;
+			else if(i == 2 || i == 3) ret[i] = 1;
+			else {  // step2 - inductive
+				min = 1 + ret[i-1];
+				if(i%3 == 0) {
+					int temp = 1 + ret[i/3];
+					min = (temp < min)? temp : min;
 				}
+				if(i%2 == 0) {
+					int temp = 1 + ret[i/2];
+					min = (temp < min)? temp : min;
+				}
+				ret[i] = min;
 			}
-			ret[cnt][2] = min;
 		}
 		// output
-		System.out.println(cnt);
-		sc.close();
+		System.out.println(ret[n]);
 	}
 }
