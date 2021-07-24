51810ff9dcd859dc1643eeed1d53f8984d6d8f02
jihyunhillpark <girl9505@naver.com>
2021-07-25 01:05:08 +0900

    code : BOJ 9012 괄호

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         //입력
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        char[] input = in.readLine().toCharArray(); // target string
-        char[] cmp = in.readLine().toCharArray();
-        int last = cmp.length-1;
-        Stack<Character> mismatchedStack = new Stack<>();
+        int N = Integer.parseInt(in.readLine()), x , y, count = 0, pre=0;
+        StringTokenizer st;
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < input.length ; i++){
-            if(input[i] == cmp[last] && cmp.length-1 <= i) {
-                int offset = 1;
-                char[] tmp = new char[cmp.length];
-                tmp[last] = cmp[last];
-                for(; offset <= last && !mismatchedStack.isEmpty() ; offset++){ // 뒤에서부터 비교하는데 offset은 몇 칸 떨어졌는지에 대한 정보
-                    if( mismatchedStack.peek() != cmp[last-offset]) break; // stack에 있는 바로 직전에 있는 다르다면 비교 중지
-                    else{
-                        tmp[last-offset] = mismatchedStack.pop(); //stack에서 pop하고, tmp 배열 끝에서 부터 매핑하여 저장한다.
-                    }
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(in.readLine(), " ");
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            if (!stack.isEmpty()) {
+                if(y < stack.peek()){
+                    count++;
+                    stack.pop();
+                    if(!stack.isEmpty() && stack.peek() == y ) continue;
+                    if( y > 0) stack.push(y);
                 }
-                if( --offset < last ) { //다른 경우 - 다시 스텍에 집어 넣는다..
-                    for (; 0 <= offset; offset--) { // 앞에서부터 다시 stack에 차례대로 넣는다.
-                        mismatchedStack.push(tmp[last - offset]);
-                    }
-                }
-            } else mismatchedStack.push(input[i]);
-        }
-
-        if(0 < mismatchedStack.size() ){
-            char[] ret = new char[mismatchedStack.size()];
-            for(int i = ret.length -1 ; 0 <= i; i-- ){
-                ret[i] = mismatchedStack.pop();
+                else if( y > stack.peek()) stack.push(y);
             }
-            System.out.println(ret);
+            else if( 0 < y ) stack.push(y);
         }
-        else System.out.println("FRULA");
+        while(!stack.isEmpty()){
+            stack.pop();
+            count++;
+        }
+        System.out.println(count);
     }
 }
