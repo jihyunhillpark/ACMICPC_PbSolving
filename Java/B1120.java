827c07cd4397530b1f5d57d40af2e9da49760bd9
jihyunhillpark <girl9505@naver.com>
2021-07-25 17:34:55 +0900

    code : BOJ 1120 문자

--- Java/B1120.java
+++ Java/B1120.java
@@ -1,10 +1,6 @@
-package BOJ2159
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
