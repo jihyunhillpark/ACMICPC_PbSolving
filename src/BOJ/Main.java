b3c0c26a1feb1a048310cb69f1d65d14e761beaa
jihyunhillpark <girl9505@naver.com>
2021-08-21 21:54:31 +0900

    code : BOJ 2210 숫자판 점프

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -3,51 +3,47 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    static HashSet<Integer> set = new HashSet<>();
+    static int map[][] = new int [5][5];
+    static int dx[] = { -1, 0, 1, 0}; // 위, 우, 하, 좌
+    static int dy[] = { 0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
-
+        // 입력 처리
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        String fast = "zzzzzzzzzzzzzzzzzzzz";
-        char crossWord[][] = new char[R][C];
+        StringTokenizer st;
 
-        for(int i = 0 ; i < R ; i++)
-            crossWord[i] = in.readLine().toCharArray();
-
-        // 행부터 차근차근
-        for(int i = 0 ; i < R ; i++){
-            String temp = "";
-            for(int j = 0 ; j < C ; j++){
-                if( crossWord[i][j] == '#' || (j == C-1) ){
-                    if(j == C-1 && crossWord[i][j] != '#') temp += crossWord[i][j]; // #가 아닌 마지막 문자는 temp에 붙여줘야 함
-                    if( 1 < temp.length() && temp.compareTo(fast) < 0) {
-                        fast = temp;
-                    }
-                    temp = "";
-                }
-                else temp += crossWord[i][j];
+        for(int i = 0 ; i < 5 ; i ++){
+            st = new StringTokenizer(in.readLine(), " ");
+            for(int j = 0 ; j < 5 ; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //열부터 차근차근
-        for(int j = 0 ; j < C ; j++){
-            String temp = "";
-            for(int i = 0 ; i < R ; i++){
-                if( crossWord[i][j] == '#' || (i == R-1) ){
-                    if(i == R-1 && crossWord[i][j] != '#') temp += crossWord[i][j];
-                    if( 1 < temp.length() && temp.compareTo(fast) < 0) {
-                        fast = temp;
-                    }
-                    temp = "";
-                }
-                else temp += crossWord[i][j];
+        // DFS
+        for(int i = 0 ; i < 5 ; i ++){
+            for(int j = 0 ; j < 5 ; j++){
+                dfs(i, j, 1, map[i][j]);
             }
         }
-        System.out.println(fast);
+        System.out.println( set.size());
+    }
+    static void dfs(int r, int c, int level, Integer sum){
+        if( level == 6 ) {
+            if(!set.contains(sum)) set.add(sum);
+            return;
+        }
+        for( int i = 0 ; i < 4 ; i++ ){
+            int nextR = r + dx[i];
+            int nextC = c + dy[i];
+            if( nextR < 0 || 5 <= nextR || nextC < 0 || 5 <= nextC) continue;
+            dfs(nextR, nextC, level+1, sum*10 + map[nextR][nextC] );
+        }
+
     }
 }
