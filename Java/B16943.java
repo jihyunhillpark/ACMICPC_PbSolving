18100b534dada68ee0968fd10840704ad6bd1c23
jihyunhillpark <girl9505@naver.com>
2021-10-13 23:51:34 +0900

    code : boj 16943 숫자 재배치

---
+++ Java/B16943.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    static int A[];
+    static int N;
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        String s = st.nextToken();
+        A = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) A[i] = s.charAt(i) - '0';
+        N = A.length;
+        int B = Integer.parseInt(st.nextToken());
+
+        // next permutation
+        int max = -1;
+        Arrays.sort(A);
+        do{
+            if(A[0] !=0){
+                int temp=0;
+                for(int i = 0; i < N; i++)
+                    temp += A[i]*(int)Math.pow(10, N-1-i);
+                if(temp < B) max = temp;
+                else break;}
+        }while(nextPermutation());
+        System.out.println(max);
+    }
+    private static boolean nextPermutation() {
+        int i = N-1;
+        while( i > 0 && A[i-1] >= A[i]) --i;
+
+        if(i ==0) return false;
+
+        int j = N-1;
+        while(A[i-1] >= A[j]) --j;
+        swap(i-1,j);
+
+        int k = N-1;
+        while(i < k){
+            swap(i++,k--);
+        }
+        return true;
+    }
+
+    private static void swap(int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
