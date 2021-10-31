b29f1daa8af51fff18793996e770619bb60b85bb
jihyunhillpark <girl9505@naver.com>
2021-10-31 14:30:14 +0900

    code : boj 16943 숫자 재배치 next permutation

--- Java/B16943.java
+++ Java/B16943.java
@@ -1,17 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Stream;
-
-public class Main {
-    static int A[];
-    static int N;
-    public static void main(String[] args) throws IOException {
-        // 입력
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+import java.io.Buffe
         String s = st.nextToken();
         A = new int[s.length()];
         for (int i = 0; i < s.length(); i++) A[i] = s.charAt(i) - '0';
